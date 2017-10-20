78d08f01fc738f3bf7fdd8709f3bc78b4a7328e1
Marco <estebanm297@gmail.com>
2017-10-19 20:14:11 -0400

    Reformatted register user with new address and added two test for it

--- src/providers/user/user.spec.ts
+++ src/providers/user/user.spec.ts
@@ -27,23 +27,27 @@ describe('MockBackend UserProvider', () => {
     this.userProvider.login("test", "password");
 
     expect(this.lastConnection).toBeDefined('no http service connection at all?');
-    expect(this.lastConnection.request.url).toMatch(/api\/users\/token$/, 'url invalid');
+    expect(this.lastConnection.request.url).toMatch(/api\/user\/token$/, 'url invalid');
   });
 
   // ensure login constructs User model from token in response.
   it('login() should return a user model', fakeAsync(() => {
     let result: User;
 
+    // Sets callback on Promise to assign returned user to result.
     this.userProvider.login("test", "password").then((user: User) => result = user);
 
+    // mockRespond prepares a fake response to be sent to the userProvider.
     this.lastConnection.mockRespond(new Response(new ResponseOptions({
       body: JSON.stringify({
           token: "token"
         }),
     })));
 
+    // tick forces Promise to resolve.
     tick();
 
+    // check assertions on result object.
     expect(result.username).toEqual("test", ' username should be test');
     expect(result.token).toEqual("token", ' token should be token');
   }));
@@ -74,19 +78,25 @@ describe('MockBackend UserProvider', () => {
     this.userProvider.register("test_username", "test_password");
 
     expect(this.lastConnection).toBeDefined('no http service connection at all?');
-    expect(this.lastConnection.request.url).toMatch(/api\/users\/create$/, 'url invalid');
+    expect(this.lastConnection.request.url).toMatch(/api\/user$/, 'url invalid');
   });
 
 
-  // ensure register constructs User.
-  it('register() should return a user', fakeAsync(() => {
-    let result: User;
+  // ensure register constructs new User.
+  it('register() should return true', fakeAsync(() => {
+    let result: Boolean;
 
-    this.userProvider.register("test", "password").then((user: User) => result = user);
+    this.userProvider.register("name", "password", "email").then((response: Boolean) => result = response);
 
+    this.lastConnection.mockRespond(new Response(new ResponseOptions({
+      body: JSON.stringify({
+          id: 1,
+          username: "name"
+        }),
+    })));
+    
     tick();
 
-    expect(result.username).toEqual("test", ' username should be test');
-    expect(result.password).toEqual("password", ' password should be password');
+    expect(result).toEqual(true);
   }));
 });
