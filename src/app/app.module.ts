12baf19e2ee4687599c51ceca6da0b5298bcb3db
abetlen <abetlen@gmail.com>
2017-10-30 17:05:38 -0400

    Add initial google maps view to home page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { GoogleMaps } from '@ionic-native/google-maps';
+import { Geolocation } from '@ionic-native/geolocation';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -27,6 +29,8 @@ import { GeolocationProvider } from '../providers/geolocation/geolocation';
   providers: [
     StatusBar,
     SplashScreen,
+    GoogleMaps,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     UserProvider,
     CommunityResourceProvider,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,72 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+ } from '@ionic-native/google-maps';
+
+import { Platform } from 'ionic-angular';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  map: GoogleMap;
+  mapElement: HTMLElement;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private googleMaps: GoogleMaps, public platform: Platform) {
+    platform.ready()
+      .then(() => {
+        this.loadMap();
+      })
+  }
 
+  loadMap() {
+    this.mapElement = document.getElementById('map');
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+
+    this.map = this.googleMaps.create(this.mapElement, mapOptions);
+
+    // Wait the MAP_READY before using any methods.
+    this.map.one(GoogleMapsEvent.MAP_READY)
+      .then(() => {
+        console.log('Map is ready!');
+
+        // Now you can use all methods safely.
+        this.map.addMarker({
+            title: 'Ionic',
+            icon: 'blue',
+            animation: 'DROP',
+            position: {
+              lat: 43.0741904,
+              lng: -89.3809802
+            }
+          })
+          .then(marker => {
+            marker.on(GoogleMapsEvent.MARKER_CLICK)
+              .subscribe(() => {
+                alert('clicked');
+              });
+          });
+
+      });
   }
 
 }
