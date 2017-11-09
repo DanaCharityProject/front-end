23e827044653215e8535f7a7426df4eae673bb7c
Alison <alison.tong@mail.utoronto.ca>
2017-11-09 01:48:01 -0500

    Basic UI with alert popup when the button is clicked and the passwords match

--- src/pages/account-settings/account-settings.ts
+++ src/pages/account-settings/account-settings.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the AccountSettingsPage page.
@@ -15,6 +16,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AccountSettingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formIn = {
+    name: '',
+    p1: '',
+    p2: ''
+  }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+  }
+    
+  saveInfo() {
+    if(this.formIn.p1 == this.formIn.p2){
+      console.log("MATCH");
+      console.log("Name: " + this.formIn.name);
+      console.log("P1: " + this.formIn.p1);
+      console.log("P2: " + this.formIn.p2);
+      const alert = this.alertCtrl.create({
+        title: 'Saved!',
+        subTitle: 'Your changes were recorded successfully.',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
   }
 }
