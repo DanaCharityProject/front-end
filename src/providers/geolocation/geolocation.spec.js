e8f2d6ab7d594f47545b7b6c8f6bfe7a2958fead
abetlen <abetlen@gmail.com>
2018-06-05 13:35:28 -0400

    Removed unnecessary .js and .js.map files from source directory.

--- src/providers/geolocation/geolocation.spec.js
+++
@@ -1,8 +0,0 @@
-var _this = this;
-import { GeolocationProvider } from './geolocation';
-describe('MockBackend GeolocationProvider', function () {
-    beforeEach(function () {
-        _this.geolocationProvider = _this.injector.get(GeolocationProvider);
-    });
-});
-//# sourceMappingURL=geolocation.spec.js.map
