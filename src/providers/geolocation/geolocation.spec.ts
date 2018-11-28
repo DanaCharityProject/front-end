9170277c7277a67685f428bee7ee19f7c9941646
abetlen <abetlen@gmail.com>
2018-11-27 19:46:20 -0500

    Cleared old source files.

--- src/providers/geolocation/geolocation.spec.ts
+++
@@ -1,9 +0,0 @@
-import {GeolocationProvider} from './geolocation'
-
-describe('MockBackend GeolocationProvider', () => {
-
-  beforeEach(() => {
-    this.geolocationProvider = this.injector.get(GeolocationProvider);
-  });
-
-})
