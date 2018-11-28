fdd6272b732f18230d6d0be5550c81bf25fa56ff
abetlen <abetlen@gmail.com>
2018-11-28 05:01:21 -0500

    Added initial vuetify theme.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,12 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
 
 Vue.use(Vuetify, {
   iconfont: 'md',
-})
+  theme: {
+    primary: '#f3254c',
+    secondary: '#1ba39d',
+    accent: '#8c9eff',
+    error: '#b71c1c',
+  },
+});
